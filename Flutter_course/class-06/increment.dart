2d3fdff8bfea40ddcf426b2e81f3a2765274d149
Hassan9865 <syedhassanraza0311@gmail.com>
2023-06-27 11:47:50 +0500

    test

---
+++ Flutter_course/class-06/increment.dart
@@ -0,0 +1,18 @@
+void main(){
+//   // pre incremet
+//   var abc = 0;
+//   var a = ++abc;
+//   print("a value: $a");
+//   print("abc value: $abc");
+// }
+
+//  // post incremet
+//   var abc = 0;
+//   var a = abc++;
+//   print("a value: $a");
+//   print("abc value: $abc");
+// }
+
+//Example
+var abc = 0;
+var a = abc--
---
+++ Flutter_course/class-06/loops.dart
@@ -0,0 +1,1 @@
+List numbers =[2,3,4]
