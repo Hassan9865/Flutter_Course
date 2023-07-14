b80bf93825bcafe5cd2ede11c169156c0f4d742e
Hassan9865 <syedhassanraza0311@gmail.com>
2023-07-14 23:25:54 +0500

    test

--- Flutter_classes/class-08,09/function.dart
+++ Flutter_classes/class-08,09/function.dart
@@ -1,24 +1,64 @@
-void main(){
-  studentRecord();
+               /// simple program ///
+     
+    //  void main (){
+    //   dartFunctiion();
+    //  }
+    //  dartFunctiion(){
+    //   print("hello! programmer");
+    //  }
+
+
+              /// parameter function ///
+              
+    // void main(){
+    //   int num1 = 2;
+    //   int num2 = 4;
+    //   int result = sumNumber(num1,num2);
+    //   print(result);
+    // }
+    // int sumNumber(int a,int b){
+    //   return a+b;
+    // }
+    
+
+
+void main() {
+  int number =5;
+  int result = factorial(number);
+  print(result);
 }
 
-studentRecord(){
-  String name = 'Hassan';
-  int age = 20;
+int factorial (int n){
+  if(n==0){
+    return 1;
+  }
+  else{
+    int num =1;
+    for(int i=1; i<=n; i++){
+      num *=i;
+    }
+    return num;
+  }
+}
 
+          /// eve or odd program ///
+  
+  // void main (){
+  //   int number =5;
+  //   bool isEven = checkEvenOdd(number);
+  //   if (isEven){
+  //     print('$number is even');
+  //   }
+  //   else{
+  //     print('$number is odd');
+  //   }
+  // }
 
-
-
-      
-  String Grade = 'A';                 
-
-
-
-
-     
-     String location ='karachi' ;
-  print(name);
-  print(age);
-  print(Grade);
-  print(location);
-} 
+  // bool checkEvenOdd(int n){
+  //   if ( n % 2 == 0){
+  //     return true;
+  //   }
+  //   else{
+  //     return false;
+  //   }
+  // }
